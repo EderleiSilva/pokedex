4b4ba26f11dd0ad974304427a41db96b17eb2d96
Ederlei Silva <ederleipereiras1@hotmail.com>
2024-12-19 12:26:15 -0300

    Montada pagina de erro 404

--- src/pages/errorPage.jsx
+++ src/pages/errorPage.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+//import React from "react";
+
+import styled from "styled-components"
 
 const ErrorPage = () => {
-    return(
-        <div>
-            <h1>Erro 404!</h1>
-        </div>
+    return (
+        <Div>
+            <h1>Ops! Algo deu errado.</h1>
+            <p>A página que você está procurando não foi encontrada.</p>
+            <img src="./src/assets/images/erro_404.png" alt="Imagem de erro 404" />
+            <a href="/">Voltar para a página inicial</a>
+        </Div>
     )
 }
+
+const Div = styled.div`
+    display: flex;
+    height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 
 export { ErrorPage }
